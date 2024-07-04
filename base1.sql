4cedd2c5859b61ce45ae265ae884cd1d8491b914
Sebas593 <sebitaspili0874@gmail.com>
2024-07-04 09:12:06 -0500

    Base de Datos1

---
+++ base1.sql
@@ -0,0 +1,95 @@
+CREATE DATABASE IF NOT EXISTS laboratorio_db;
+USE laboratorio_db;
+
+CREATE TABLE empleados(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50),
+    departamento_id INT,
+    salario DECIMAL(10,2)
+);
+
+CREATE TABLE departamento(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nombre VARCHAR(50)
+);
+
+INSERT INTO empleados(nombre,departamento_id,salario)VALUES('Sebas',1,1000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
